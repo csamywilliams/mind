f6c829168a821eec7c6dc3f71218f40865d2c795
Amy Williams <csamywilliams@gmail.com>
2021-07-24 17:13:04 +0100

    Added tile row organism

---
+++ src/components/UI/organisms/tile-row/index.tsx
@@ -0,0 +1,16 @@
+import Tile from "../../molecules/tile";
+import TileProps from "../../../interfaces/tile-props"
+
+import { TileRowStyled } from "./tile-row.styled"
+
+type TileRowProps = {
+  tiles: Array<TileProps>
+}
+
+const TileRow = ({ tiles }: TileRowProps) => <div>
+  <TileRowStyled>
+    {tiles.map(({name, icon, page, styledAs}) => <Tile key={name} name={name} icon={icon} page={page} styledAs={styledAs} />)}
+  </TileRowStyled>
+</div>
+
+export default TileRow;
---
+++ src/components/UI/organisms/tile-row/tile-row.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const TileRowStyled = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-content: center;
+	justify-content: center;
+	align-items: center;
+`;
