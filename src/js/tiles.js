7b504ebc6359ac93bd2cbf22fcbfedfe3d39b29b
Amy Williams <csamywilliams@gmail.com>
2021-07-24 17:13:21 +0100

    Added tile object data

---
+++ src/js/tiles.js
@@ -0,0 +1,19 @@
+import { SiLivejournal } from 'react-icons/si';
+import { GiStairsGoal } from 'react-icons/gi';
+
+const tiles = [
+	{
+		name: 'Journal',
+		page: 'journal',
+		icon: SiLivejournal,
+		styledAs: 'journal',
+	},
+	{
+		name: 'Goals',
+		page: 'goals',
+		icon: GiStairsGoal,
+		styledAs: 'goals',
+	},
+];
+
+export default tiles;
